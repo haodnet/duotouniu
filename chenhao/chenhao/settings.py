c192274f2b186e9ab7aea5bd41e3a118d734212a
chenhao <haodnet@163.com>
2018-11-16 12:46:49 +0800

    临时

--- chenhao/chenhao/settings.py
+++ chenhao/chenhao/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'chenhao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'NAME': 'db_test',
     }
 }
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
